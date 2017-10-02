447e18459259cb20caa2763bd9feaf2e97491a44
nahuel <nahuelcorreo666@hotmail.com.ar>
2017-10-02 20:52:31 -0300

    Practico mayor - base

---
+++ PracticoMayor/src/practicomayor/PracticoMayor.java
@@ -0,0 +1,17 @@
+
+package practicomayor;
+
+import vista.PracticoMayorVista;
+
+
+public class PracticoMayor {
+
+    
+    public static void main(String[] args) {
+        
+        PracticoMayorVista miVentana = new PracticoMayorVista();
+        miVentana.setVisible(true);
+        
+    }
+    
+}
