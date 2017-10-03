1f616384b09a6e4fbb5ccf4a90a3b7da91c19fdc
nahuel <nahuelcorreo666@hotmail.com.ar>
2017-10-02 21:52:48 -0300

    Iniciando el practico

---
+++ PracticoMayor2/src/practicomayor2/PracticoMayor2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicomayor2;
+
+import vista.PracticoMayorVista2;
+
+/**
+ *
+ * @author Monica
+ */
+public class PracticoMayor2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        PracticoMayorVista2 miVentana = new PracticoMayorVista2();
+        miVentana.setVisible(true);
+    }
+    
+}
