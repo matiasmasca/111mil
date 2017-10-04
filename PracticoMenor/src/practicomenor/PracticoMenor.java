e0b404919139c8b5a1c60fc94ba8e39d08542d25
Rocio <rocio@gmail.com>
2017-10-04 20:38:43 -0300

    Practico menor, version inicial

---
+++ PracticoMenor/src/practicomenor/PracticoMenor.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicomenor;
+
+import vista.PracticoMenorVista;
+
+/**
+ *
+ * @author Monica
+ */
+public class PracticoMenor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        PracticoMenorVista miVentana = new PracticoMenorVista();
+        miVentana.setVisible(true);
+    }
+    
+}
