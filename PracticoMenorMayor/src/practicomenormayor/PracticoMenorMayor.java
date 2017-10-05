9c0f6c4a7b439568ffaaf834e963bcc1b5d15aed
Rocio <rocio@gmail.com>
2017-10-04 21:30:53 -0300

    Practico MenorMayor base

---
+++ PracticoMenorMayor/src/practicomenormayor/PracticoMenorMayor.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicomenormayor;
+
+import vista.PracticoMenorMayorVista;
+
+/**
+ *
+ * @author Monica
+ */
+public class PracticoMenorMayor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       PracticoMenorMayorVista miVentana = new PracticoMenorMayorVista();
+        miVentana.setVisible(true);
+    }
+    
+}
