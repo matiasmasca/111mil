f3c303cce45bcd1ab6652c54449bd7423387b1b6
Rocio <rocio@gmail.com>
2017-10-06 21:49:02 -0300

    Commit inicial del practico

---
+++ PracticoOrdenador2/src/practicoordenador2/PracticoOrdenador2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicoordenador2;
+
+import vista.PracticoOrdenadorVista2;
+
+/**
+ *
+ * @author Monica
+ */
+public class PracticoOrdenador2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       PracticoOrdenadorVista2 miVentana = new PracticoOrdenadorVista2();
+        miVentana.setVisible(true);
+    }
+    
+}
