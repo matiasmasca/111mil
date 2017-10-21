aceee507256df19feea98b8ef5fa4e2e5d90f17e
nahuel <nahuelcorreo666@hotmail.com.ar>
2017-10-21 12:21:20 -0300

    primera version

---
+++ ProyectoSueldo/ProyectoSueldo/src/proyectosueldo/ProyectoSueldo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectosueldo;
+import vista.SueldosVista;
+
+/**
+ *
+ * @author alumno
+ */
+public class ProyectoSueldo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SueldosVista miVentana = new SueldosVista();
+        miVentana.setVisible(true);
+    }
+    
+}
+ 
